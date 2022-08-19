0e927d3a0c2e39a290df631312525f7e20d59939
ronentgreat <ronenh53@gmail.com>
2022-08-19 19:44:43 +0000

    added shortened input

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ logo_img = PhotoImage(file="cut.png")
 canvas.create_image(100, 90, image=logo_img)
 canvas.grid(column=1, row=0)
 
-# URL Input
-url_label = Label(text="URL: ")
-url_label.grid(column=0, row=1)
-url_input = Entry(width=25)
-url_input.grid(column=1, row=1)
+# Long URL Input
+long_url_label = Label(text="URL: ")
+long_url_label.grid(column=0, row=1)
+long_url_input = Entry(width=25)
+long_url_input.grid(column=1, row=1)
+
+# Short URL input
+short_url_label = Label(text="Shortened URL: ")
+short_url_label.grid(column=0, row=3)
+shorted_url = Entry(width=25)
+shorted_url.grid(column=1, row=3)
 
 window.mainloop()
 
