0e3874a6c4a694da0ed8b7fbba0ea2230e02e67f
ronentgreat <ronenh53@gmail.com>
2022-08-19 19:38:42 +0000

    added logo and input for url

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import pyshorteners
 from tkinter import *
 
+# Window Setup
+window = Tk()
+window.title("URL Shortener")
+window.config(padx=30, pady=30)
+
+# Canvas Setup
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="cut.png")
+canvas.create_image(100, 90, image=logo_img)
+canvas.grid(column=1, row=0)
+
+# URL Input
+url_label = Label(text="URL: ")
+url_label.grid(column=0, row=1)
+url_input = Entry(width=25)
+url_input.grid(column=1, row=1)
+
+window.mainloop()
+
 long_url = input("Enter the URL to shorten: ")
 print(long_url)
 
